6af748ce8bec945d2cb37eab0f4498c00a4e71a3
Kafi09 <kaficsmy9@gmail.com>
2022-10-12 19:12:54 +0600

    added bar chart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -10,7 +10,24 @@ const Statistics = () => {
         <div>
             <section>
                 <div>
-                   
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={quiz}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#82ca9d" />
+                    </BarChart>
                 </div>
             </section>
 
