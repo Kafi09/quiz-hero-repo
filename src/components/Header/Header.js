7befd7b60e1398484947bae86520ef4e25a0c3b7
Kafi09 <kaficsmy9@gmail.com>
2022-10-12 19:09:56 +0600

    added header final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,22 @@ import './Header.css';
 
 const Header = () => {
     return (
-       
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand>Quiz Hero</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    
+                    <div className='nav-bar'>
+                        <Nav>
+                            <Link className='lip' to='/'>Home</Link>
+                            <Link className='lip' to='Statistics'>Statistics</Link>
+                            <Link className='lip' to='/Blog'>Blog</Link>
+                        </Nav>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
