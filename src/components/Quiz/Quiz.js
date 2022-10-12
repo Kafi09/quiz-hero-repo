45cc3e95d59215287e0dc909e734373cb9e5c25d
Kafi09 <kaficsmy9@gmail.com>
2022-10-12 19:11:39 +0600

    added quiz final

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quizzz from './Quizzz/Quizzz';
 
-
+const Quiz = () => {
+    const name = useLoaderData().data;
+    const question = useLoaderData().data.questions;
+    console.log(question);
+    return (
+        <div>
+            <h3>Exam Name: {name.name}</h3>
+            {
+                question.map(quizzz => <Quizzz
+                    key={quizzz.name}
+                    quizzz={quizzz}
                 >
                 </Quizzz>)
             }
