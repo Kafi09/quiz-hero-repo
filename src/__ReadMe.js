47a0129730aad2aae97d897652444172aeb4a6a0
Kafi09 <kaficsmy9@gmail.com>
2022-10-12 21:40:20 +0600

    added readme

--- src/__ReadMe.js
+++ src/__ReadMe.js
@@ -1,3 +1,8 @@
 /*
-
+1.Project Name: "Quiz Hero" website for quiz test
+2.Live Site Link: https://exquisite-babka-176dbb.netlify.app
+3.This website is for:
+    *quiz test for learners
+    *learning web development topics
+    *this website build by React Bootstrap
 */
